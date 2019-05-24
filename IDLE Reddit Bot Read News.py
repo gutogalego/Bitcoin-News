10f3e6dad56b80aca2f2fea9a07a5db50e3c0bdb
gutogalego <augustomirandagalego@gmail.com>
2019-05-24 13:48:06 -0300

    edited prin + sends empty mail

--- IDLE Reddit Bot Read News.py
+++ IDLE Reddit Bot Read News.py
@@ -8,16 +8,12 @@ from bs4 import BeautifulSoup
 import nltk
 import smtplib, ssl
 
-# Create the Reddit instance and log in
-reddit = praw.Reddit(
-    client_id='vhCE7aeRg4MAJg',
-    client_secret='SAusJVyuClMrejHq_HrO3m1lovA',
-    #password='guto0502',
-    #username='Apprehensive_Design',
-    user_agent='L1pzBl4ckDr4g0n'
-)
+
+global mail
+mail = ''
 
 
+#'Main' method. Gets hot subs, selects top 5, sends email
 def get_hot(subreddit_name):
 
     # Get hot 15 submissions from reddit
@@ -40,7 +36,6 @@ def get_hot(subreddit_name):
         if(counter==5):
             break
     print('\n NArcticles: ' + str(counter))
-    #send_mail(mail)
 
 
 def display_info(submission, score):
@@ -50,13 +45,10 @@ def display_info(submission, score):
         article = Article(submission.url)
         article.download()
         article.parse()
-        print('\n\nARC: ')
-        print(article.url)
-        print('\n')
-        print(article.title)
         article.nlp()
-        print('\n')
-        print(article.summary)
+        text_to_print = '\n\n\n Art with score '+str(submission.score)+' : ' + article.title +'\n\n'+ article.summary + '\nlink:' + article.url
+        #global mail = mail + text_to_print
+        print(text_to_print)
 
 
 def score_submission(submission):
@@ -65,13 +57,47 @@ def score_submission(submission):
             return 0
     return 1
 
-#def send_mail(mail):
+def send_mail(mail):
+
+    password_file = r'C:\Users\Augusto\Documents\projects\PetProjects\password.txt'
+    file = open(password_file,'r')
+    password = file.read()
+    
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = "augusto.m.galego@gmail.com"
+    receiver_email = "augustomirandagalego@gmail.com"
+    message = """\
+    Subject: Hi there
+
+    This message is sent from Python."""
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
     
 
-subreddits = ['Bitcoin', 'CryptoCurrency']
-banned_substances = ['i.redd.it', 'png', 'jpg', 'imgur', 'youtu', 'daily_discussion','reddit']
-       
-for sr in subreddits:
-    print('\n\nScraping /r/%s...' % sr)
-    get_hot(sr)
 
+
+def main():
+    
+    subreddits = ['Bitcoin', 'CryptoCurrency']
+    banned_substances = ['i.redd.it', 'png', 'jpg', 'imgur', 'youtu', 'daily_discussion','reddit']
+
+    # Create the Reddit instance and log in
+    reddit = praw.Reddit(
+        client_id='vhCE7aeRg4MAJg',
+        client_secret='SAusJVyuClMrejHq_HrO3m1lovA',
+        #password='',
+        #username='',
+        user_agent='L1pzBl4ckDr4g0n'
+)
+
+    
+    for sr in subreddits:
+        print('\n\nScraping /r/%s...' % sr)
+        mail =''
+        get_hot(sr)
+        print(mail)
+    send_mail('')
